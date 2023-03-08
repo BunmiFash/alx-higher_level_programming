3709e82f024934c75ff1c02b9646de1e1baaef21
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-08 00:32:47 +0000

    Fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
     for i in range(1, 101):
         if (i % 15 == 0):
             print("{} ".format("FizzBuzz"), end="")
-        if (i % 3 == 0):
+        elif (i % 3 == 0):
             print("{} ".format("Fizz"), end="")
         elif (i % 5 == 0):
             print("{} ".format("Buzz"), end="")
