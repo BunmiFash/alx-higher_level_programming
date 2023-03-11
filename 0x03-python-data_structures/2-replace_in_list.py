e643b3333b65d4b50d13b1bbe9d3b4e54b366a0b
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-11 16:15:08 +0000

    Replace: removed trailing whitespace again

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,7 +6,6 @@ def replace_in_list(my_list, idx, element):
     last_index = len(my_list) - 1
     if (idx < 0) or (idx > last_index):
         return (my_list)
-    
     else:
         my_list[idx] = element
         return (my_list)
