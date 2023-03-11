6797e23d267825c6f4f921e374a428e64cc4dd0d
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-11 16:19:00 +0000

    Replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-# Write a function that replaces an element of a list at a specific position (like in C).
 def replace_in_list(my_list, idx, element):
     last_index = len(my_list) - 1
     if (idx < 0) or (idx > last_index):
