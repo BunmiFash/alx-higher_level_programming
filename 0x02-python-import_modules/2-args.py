bce5c12594ecf62d3cff5a35c4b425ee77e2b053
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-12 14:31:28 +0000

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
 import sys
+if __name__ == "__main__":
+    argv = sys.argv
+    arg_len = len(argv)
 
-argv = sys.argv
-arg_len = len(argv)
-count = 1
+    if (arg_len == 1):
+        print('0 arguments.')
+    elif (arg_len == 2):
+        print("{:d} argument:".format(1))
+        print("{:d}: {}".format(1, argv[1]))
 
-if (arg_len == 1):
-    print('0 arguments.')
-elif (arg_len == 2):
-    print("{:d} argument:".format(1))
-    print("{:d}: {}".format(1, argv[1]))
-
-else:
-    print("{:d} arguments:".format(arg_len - 1))
-    for i in range(1, arg_len):
-        print("{:d}: {}".format(i, argv[i]))
+    else:
+        print("{:d} arguments:".format(arg_len - 1))
+        for i in range(1, arg_len):
+            print("{:d}: {}".format(i, argv[i]))
