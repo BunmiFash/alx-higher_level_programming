34d8a07b8d70a87b88da46c1bfb2117c4c92f3f3
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-15 22:30:31 +0000

    Average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    add = 0
+    div = 0
+    for sub in my_list:
+        first = sub[0]
+        second = sub[1]
+        mul = first * second
+        add += mul
+        div += second
+    return (add / div)
