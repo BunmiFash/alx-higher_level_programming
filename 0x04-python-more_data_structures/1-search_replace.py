7311ebb9f856f9e8dbd78a33bd424a12bb0bbd43
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-15 12:45:18 +0000

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 
 def search_replace(my_list, search, replace):
     new = my_list.copy()
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
+    for i in range(len(new)):
+        if new[i] == search:
+            new[i] = replace
     return new
