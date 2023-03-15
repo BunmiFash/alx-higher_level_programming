a89e7dfad19e2a18da8fce056b58ebfd3d75291a
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-15 23:20:52 +0000

    del

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,12 +2,9 @@
 
 def complex_delete(a_dictionary, value):
     to_del = []
-    values = a_dictionary.values()
-    if value not in values:
-        return a_dictionary
     for key, val in a_dictionary.items():
-        if val == value:
-            to_del.append(key)
+       if val == value:
+           to_del.append(key)
     for key in to_del:
         del a_dictionary[key]
     return a_dictionary
