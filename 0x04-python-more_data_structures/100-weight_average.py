25709de3174ebe787019dbe899fd4ad71858ee20
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-15 22:42:26 +0000

    Fixed bool

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if len(my_list == 0):
+    if len(my_list) == 0:
         return 0
     add = 0
     div = 0
     for sub in my_list:
-        add += (sub[0] * sub[1])
-        div += sub[1]
+        score = sub[0]
+        weight = sub[1]
+        mult = score * weight
+        add += mult
+        div += weight
     return (add / div)
