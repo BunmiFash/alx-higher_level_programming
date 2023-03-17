81a6a966c0cde8441a70422273b520fc1dff8002
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-17 10:04:07 +0000

    Changes

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     keys = sorted(a_dictionary.keys())
-    for key in keys:
-        print("{} : {}".format(key, a_dictionary[key]))
+    values = sorted(a_dictionary.values())
+    newKeys = []
+    if (len(a_dictionary) == 0):
+        print("a_dictionary = {}")
+    for i in keys:
+        i = i.lower()
+        newKeys.append(i)
+    newKeys = sorted(newKeys)
+
+    for key, values in zip(newKeys, values):
+        print("{} : {}".format(key, values))
