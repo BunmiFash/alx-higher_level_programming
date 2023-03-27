4d947fc02520dc6581d247112a616730dce6710a
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-27 14:52:44 +0000

    stderr

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+        return res
+    except BaseException as ex:
+        print("Exception: ", ex, file=sys.stderr)
+        return None
