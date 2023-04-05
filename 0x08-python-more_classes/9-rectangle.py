a0ae9eb2ce54b662e042d5c4e68239dabf097daa
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-04-05 11:46:03 +0000

    attr

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -88,5 +88,4 @@ class Rectangle:
     """
     @classmethod
     def square(cls, size=0):
-        """ Returns new rectangle instance with width == height == size """
         return cls(size, size)
