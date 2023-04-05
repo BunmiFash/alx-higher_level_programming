8418e012fe13fc7ef851f2b9e8f873cd495e381b
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-04-05 11:39:19 +0000

    Fix new attribute

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -83,7 +83,9 @@ class Rectangle:
         else:
             return rect_2
 
-    """Method that returns a new Rectangle instance"""
+    """Method that returns a new Rectangle
+    instance with width == height == size
+    """
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        return Rectangle(size, size)
