05638ba35ffe81b53683a22ec8c3de1536aa14e4
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-04-05 00:53:23 +0000

    Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""A module that creates a class Rectangle"""
+
+
+class Rectangle:
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""A module that creates a class Rectangle
+and creates private attributes
+"""
+
+
+class Rectangle:
+    """Initializing"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    """Getter for width"""
+    @property
+    def width(self):
+        return self.__width
+
+    """Setter for width"""
+    @width.setter
+    def width(self, value):
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    """Getter for height"""
+    @property
+    def height(self):
+        return self.__height
+
+    """Setter for height"""
+    @height.setter
+    def height(self, value):
+        if not type(value) is int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
