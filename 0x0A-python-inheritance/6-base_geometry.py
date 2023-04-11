28e9a04c1f5127088f87672f4c03494e7603dfb6
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-04-11 18:58:33 +0000

    Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+"""Module that creates a Base Class"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
