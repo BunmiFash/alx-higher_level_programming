1ebcf494b10dc5ce3be2290f6e960a80bc37625c
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-04-12 19:36:50 +0000

    Doc

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,6 @@ Reads a text file
 
 
 def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout"""
     with open(filename, 'r', encoding='UTF8') as a_file:
         print(a_file.read())
