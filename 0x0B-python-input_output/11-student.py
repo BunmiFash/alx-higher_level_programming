127df381a9a68571185c00035300aca15a35a05d
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-04-12 22:40:02 +0000

    Replace

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+""" Module 9-student
+Creates a class Student
+"""
+
+
+class Student:
+    """class Student with public instance attributes"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializing the attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of the class student"""
+        if (type(attrs) == list and
+                all(type(a) == str for a in attrs)):
+            return {b: getattr(self, b) for b in attrs if hasattr(self, b)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        obj = json.items()
+        for k, v in obj:
+            setattr(self, k, v)
