0da41ab399f53797594a3092fd1f3c803365dd48
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-05-19 21:51:16 +0000

    tvshows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,8 +2,6 @@
 -- that have at least one genre linked
 
 -- CREATE DATABASE
-CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
-USE hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres
