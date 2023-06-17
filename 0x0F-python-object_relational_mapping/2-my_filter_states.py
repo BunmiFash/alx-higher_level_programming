66dbb8bdf33381f2a8f32865f696d0026f65278d
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-06-17 07:06:00 +0000

    argv[3]

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost", port=3306,  user=argv[1], password=argv[2],
-        db='hbtn_0e_0_usa')
+        db=argv[3])
     cur = db.cursor()
     cur.execute(
             """SELECT * FROM states WHERE name LIKE BINARY '{}'
