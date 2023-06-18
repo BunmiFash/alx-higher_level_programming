4ef4aa54cb098a4db05dc7e2ced0e8e0ba490eff
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-06-18 22:50:00 +0000

    all

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,8 +17,17 @@ if __name__ == "__main__":
             host="localhost", port=3306, user=argv[1],
             password=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states
-        WHERE name LIKE 'N%' ORDER BY states.id ASC""")
+    cur.execute("""
+                    SELECT
+                            *
+                    FROM
+                            states
+                    WHERE
+                            name
+                    LIKE BINARY
+                            'N%'
+                    ORDER BY
+                            states.id ASC""")
 
     states = cur.fetchall()
     for state in states:
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,9 +14,17 @@ if __name__ == "__main__":
         host="localhost", port=3306, user=argv[1],
         password=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute(
-            """SELECT * FROM states WHERE name
-            LIKE BINARY %s ORDER BY states.id ASC""", (argv[4],))
+    cur.execute("""
+                SELECT
+                        *
+                FROM
+                        states
+                WHERE
+                        name
+                LIKE BINARY
+                        %s
+                ORDER BY
+                        states.id ASC""", (argv[4],))
     states = cur.fetchall()
     for state in states:
         print(state)
