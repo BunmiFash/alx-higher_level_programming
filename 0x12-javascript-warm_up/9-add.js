706904b14669d3f9b1e8ac35a43b6cb23b5dc809
Fashina Blessing Oluwabunmi <foluwaboonmy@gmail.com>
2023-06-27 00:39:14 +0100

    add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
+/*
+if (first === undefined || second === undefined || isNaN(first) === true || isNaN(second) === true) {
+  console.log('NaN');
+} */
+console.log(first + second);
