7991115aedba2299a2a0a8b7c4a4fb0560d9704f
Fashina Blessing Oluwabunmi <foluwaboonmy@gmail.com>
2023-07-20 10:53:44 +0100

    genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -10,5 +10,5 @@ SELECT tv_genres.name AS genre, COUNT(*) as number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
+GROUP BY genre
 ORDER by number_of_shows DESC;
