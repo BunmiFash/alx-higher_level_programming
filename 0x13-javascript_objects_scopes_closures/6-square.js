f66303424d2fc886aed4f3e1372a12f34661677f
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-08-24 17:32:08 +0100

    sqr

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Sqr = require('./5-square');
+
+module.exports = class Square extends Sqr {
+  charPrint (c) {
+    const val = c;
+    if (val !== undefined) {
+      for (let row = 0; row < this.height; row++) {
+        let str = '';
+        for (let col = 0; col < this.width; col++) {
+          str += 'C';
+        }
+        console.log(str);
+      }
+    } else {
+      super.print();
+    }
+  }
+};
