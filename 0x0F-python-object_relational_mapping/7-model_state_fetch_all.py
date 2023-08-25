380f91e668164117544544be2ff9f25e0ae21fba
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-08-25 15:47:31 +0100

    state model

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+A script that lists all State objects
+from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            argv[1], argv[2], argv[3])
+    engine = create_engine(db)
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    for id, name in session.query(State.id, State.name).order_by(State.id):
+        print('{}: {}'.format(id, name))
