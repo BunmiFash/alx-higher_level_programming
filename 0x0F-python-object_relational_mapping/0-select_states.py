d83602a72a2bec0bdd9b5314a41d5c6bb2c557d0
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-08-25 11:00:15 +0100

    argv

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,13 +7,14 @@ hbtn_0e_0_usa
 
 if __name__ == "__main__":
     import MySQLdb
+    from sys import argv
 
     db = MySQLdb.connect(
-            host="localhost", port=3306, user="root",
-            passwd="Oluwabunmi12.", db="hbtn_0e_0_usa")
+            host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute(
-            """SELECT id, name
+            """SELECT *
                 FROM states
                 ORDER BY states.id ASC
             """)
