884146aaeeee14bf5469939065a254ffc6a02ee6
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-08-25 11:20:19 +0100

    sql

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+A script that takes in an argument and
+displays all values in the state table
+from the database hbtn_0e_0_usa that
+matches the argument.
+This script is safe from SQL injection
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(
+            host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute(
+            """SELECT *
+                FROM states
+                WHERE name LIKE BINARY %s
+                ORDER BY states.id ASC
+            """, (argv[4],))
+    for state in cur.fetchall():
+        print(state)
+
+    cur.close()
+    db.close()
