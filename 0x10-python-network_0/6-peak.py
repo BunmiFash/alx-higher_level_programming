47c9b6394b6baa373be9c73773da6c8a5fcd8cb8
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-08-31 20:13:27 +0100

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,19 +8,20 @@ def find_peak(list_of_integers):
     """
     Find peak
     """
-    """
     _list = list_of_integers[:]
     length = len(_list)
     if length == 0:
-        return None
-    else:
-        first_peak = _list[0]
-        peak = _list[0]
-        for i in range(length - 1):
-            if _list[i] >= _list[i + 1] and _list[i] >= _list[i - 1]:
-                peak = _list[i]
-        return peak
+        return
+    first_peak = _list[0]
+    peak = _list[0]
+    for i in range(length - 1):
+        if (_list[i] >= _list[i + 1] and _list[i] >= _list[i - 1]):
+            peak = _list[i]
+    if first_peak > peak:
+        peak = first_peak
+    return peak
     """
     if len(list_of_integers) > 1:
         return max(list_of_integers)
     return
+    """
