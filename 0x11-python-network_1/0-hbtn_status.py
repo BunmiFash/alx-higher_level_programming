f5600b771794d29ede0e34d5fae2473f5883c010
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-09-02 23:06:33 +0100

    url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as resp:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         response = resp.read()
         print('Body response:')
         print('\t- type: ', response.__class__)
