e33f65924a2c1a08d5a8e26a90aa6f1fb588b46f
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-09-02 22:48:16 +0100

    urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as resp:
+        response = resp.read()
+        print('Body response:')
+        print('\t- type: ', response.__class__)
+        print('\t- content: ', response)
+        print('\t- utf content: ', response.decode('utf-8'))
