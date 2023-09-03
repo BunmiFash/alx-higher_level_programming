3b8787789d1f0346f5bd8ee8d9edaf576d833e20
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-09-03 01:00:19 +0100

    error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL sends a request
+to the passed URL and displays the body of
+the response (decoded in utf-8)
+"""
+import urllib.request
+import urllib.parse
+import sys
+import urllib.error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as resp:
+            response = resp.read()
+            response = response.decode('utf-8')
+            print(response)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}", format(e.code))
