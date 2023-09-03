fccdf84fb08035365808af3bb8c3ba8c56a68fb5
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-09-03 01:40:56 +0100

    status1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         response = resp.read()
-        print('Body response:')
-        print('\t- type: ', response.__class__)
-        print('\t- content: ', response)
-        print('\t- utf8 content: ', response.decode('utf-8'))
+        print("Body response:")
+        print("\t- type: {}".format(response.__class__))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode('utf-8')))
